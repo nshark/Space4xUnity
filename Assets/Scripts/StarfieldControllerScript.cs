2c3cacd5a2b5f827d0b8de254a244cec469d197e
Nathan Sitkoff <nsitkoff.ext@ext.seconddinner.com>
2025-07-06 19:41:25 -0400

    fix name of starfield controller

--- Assets/Scripts/StarfieldControllerScript.cs
+++ Assets/Scripts/StarfieldControllerScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class StanfieldControllerScript : MonoBehaviour
+public class StarfieldControllerScript : MonoBehaviour
 {
     [SerializeField] private GameObject starPrefab;
 
