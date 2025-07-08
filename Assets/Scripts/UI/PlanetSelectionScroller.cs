defc3c23bf5f6ce233d046ce168cee97f5847e80
noah@sitkoff.net <noah@sitkoff.net>
2025-07-08 17:51:58 -0400

    set planet types into planet menu

--- Assets/Scripts/UI/PlanetSelectionScroller.cs
+++ Assets/Scripts/UI/PlanetSelectionScroller.cs
@@ -1,18 +1,28 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlanetSelectionScroller : MonoBehaviour
 {
     [SerializeField] private GameObject planetButtonPrefab;
+    [SerializeField] private GameObject scrollContent;
     
     public void Initialize(StarState state)
     {
-        foreach (var planetState in state.Planets)
+        scrollContent.GetComponent<RectTransform>().sizeDelta =
+            new Vector2(scrollContent.GetComponent<RectTransform>().sizeDelta.x, 120 * state.Planets.Length);
+        Assert.IsTrue(state.Planets.Length <= scrollContent.transform.childCount, "Too many Planets. Add more prefabs");
+        for (int i = 0; i < state.Planets.Length; i++)
         {
-            GameObject g = Instantiate(planetButtonPrefab, transform);
-            g.GetComponentInChildren<TextMeshProUGUI>().text = planetState.Type;
+            GameObject g = scrollContent.transform.GetChild(i).gameObject;
+            g.GetComponentInChildren<TextMeshProUGUI>().text = state.Planets[i].Type;
+            g.SetActive(true);
+        }
+
+        for (int i = state.Planets.Length; i < scrollContent.transform.childCount; i++)
+        {
+            scrollContent.transform.GetChild(i).gameObject.SetActive(false);
         }
     }
-
 }
